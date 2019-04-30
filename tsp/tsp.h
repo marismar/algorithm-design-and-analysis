c0f29adefb8c83d690d7d8d2e6d128f1c86cedab
Marismar da Costa Silva <cmarismar@gmail.com>
2019-04-30 16:35:34 -0300

    Update tsp.h

--- tsp/tsp.h
+++ tsp/tsp.h
@@ -14,18 +14,24 @@ typedef struct grafo{
     int **elementos;
 };
 
+/*typedef struct solucao{
+    int custo;
+    int *rota;
+};*/
 
-/* FUNCOES ESSENCIAIS */
+
+/*------ FUNCOES ESSENCIAIS ------*/
 void iniciaGrafo(grafo *g, ifstream &instancia);
 void HVMP(grafo g, int *rota, int n_inicial);
 int *opt2(grafo g, int *rota);
 
 
 /*------ FUNCOES AUXILIARES ------*/
+static void flip(int n_elementos, int *rota, int *rt_aux, int lim1, int lim2);
+static void copiaArray(int *destino, int *origem, int n_elementos);
 int calculaCusto(grafo g, int *rota);
 int calculaNInicial(grafo g, int *rota);
 void printMatriz(grafo g);
 void printRota(grafo g, int *rota);
-
 
 #endif // TSP_H_INCLUDED
