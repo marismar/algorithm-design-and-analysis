b81ec8ad437902677d99949834cfbd59c9abe79d
Marismar da Costa Silva <cmarismar@gmail.com>
2019-05-08 01:17:55 -0300

    Update main.cpp

--- tsp/main.cpp
+++ tsp/main.cpp
@@ -9,44 +9,9 @@ int main()
 
     grafo g;
     iniciaGrafo(&g, instancia);
-    printMatriz(g);
 
-    int rota[g.n_elementos + 1];
-    HVMP(g, rota, calculaNInicial(g, rota));
-    printRota(g, rota);
+    GRASP(g, 2);
 
-    int *rt_teste = new int[g.n_elementos + 1];
-
-    rt_teste = opt2(g, rota);
-
-    printRota(g, rt_teste);
-    int a = calculaCusto(g, rt_teste);
-    cout << "custo: " << " ";
-    cout << a << endl;
-    rt_teste = swap(g, rt_teste);
-    printRota(g, rt_teste);
-    a = calculaCusto(g, rt_teste);
-    cout << "custo: " << " ";
-    cout << a << endl;
-    rt_teste = swap(g, rt_teste);
-    printRota(g, rt_teste);
-    a = calculaCusto(g, rt_teste);
-    cout << "custo: " << " ";
-    cout << a << endl;
-    rt_teste = opt2(g, rt_teste);
-
-    printRota(g, rt_teste);
-    a = calculaCusto(g, rt_teste);
-    cout << "custo: " << " ";
-    cout << a << endl;
-    cout << "OK" << endl;
-    
-    VND(g, rota);
-
-    printRota(g, rota);
-    a = calculaCusto(g, rota);
-    cout << "custo: " << " ";
-    cout << a << endl;
 
     return 0;
 }
